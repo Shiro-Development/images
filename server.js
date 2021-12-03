5031b1638dcf84b3300cab4ba399c9c79fde7fe3
Luke-6723 <luke@ichigo.uk>
2021-12-03 23:08:56 +0000

    image fetch endpoint above auth

--- server.js
+++ server.js
@@ -15,6 +15,19 @@ app.get('/health', (req, res) => {
   return res.status(200).send('Service OK')
 })
 
+app.get('/:category/:file', async (req, res) => {
+  const fileName = `/images/${req.params.category}/${req.params.file}`
+  const fileExists = await fs.stat(`./${fileName}`).catch(() => undefined)
+  if (fileExists) {
+    const file = await fs.readFile(`${__dirname}${fileName}`)
+    const mimeType = mime.contentType(req.params.file)
+    if (mimeType) res.setHeader('Content-Type', mimeType)
+    return res.send(file)
+  } else {
+    return res.status(404).json({ code: 404, message: "Category doesn't exist" })
+  }
+})
+
 app.use((req, res, next) => {
   if (req.headers?.authorization === process.env.AUTH_TOKEN) {
     next()
@@ -39,19 +52,6 @@ app.get('/:category', async (req, res) => {
     const files = await fs.readdir(dirName)
     const randomNumber = Math.round(Math.random() * await files.length)
     return res.status(200).json({ code: 200, url: `${process.env.SUB_PATH}/${req.params.category}/${files[randomNumber]}` })
-  } else {
-    return res.status(404).json({ code: 404, message: "Category doesn't exist" })
-  }
-})
-
-app.get('/:category/:file', async (req, res) => {
-  const fileName = `/images/${req.params.category}/${req.params.file}`
-  const fileExists = await fs.stat(`./${fileName}`).catch(() => undefined)
-  if (fileExists) {
-    const file = await fs.readFile(`${__dirname}${fileName}`)
-    const mimeType = mime.contentType(req.params.file)
-    if (mimeType) res.setHeader('Content-Type', mimeType)
-    return res.send(file)
   } else {
     return res.status(404).json({ code: 404, message: "Category doesn't exist" })
   }
