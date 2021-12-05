5da635b11ad42f3bcca0a4a737ba719f27217a89
Luke-6723 <luke@ichigo.uk>
2021-12-05 16:57:07 +0000

    fix nsfw category

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.get('/health', (req, res) => {
   return res.status(200).send('Service OK')
 })
 
-app.get('/:category/:file', async (req, res) => {
+app.get('/:category/:file', async (req, res, next) => {
+  if (req.params.file) return next()
   const fileName = `/images/${req.params.category}/${req.params.file}`
   const fileExists = await fs.stat(`./${fileName}`).catch(() => undefined)
   if (fileExists) {
@@ -28,11 +29,16 @@ app.get('/:category/:file', async (req, res) => {
   }
 })
 
-app.use((req, res, next) => {
-  if (req.headers?.authorization === process.env.AUTH_TOKEN) {
-    next()
+app.get('/nsfw/:category/:file', async (req, res) => {
+  const fileName = `/images/nsfw/${req.params.category}/${req.params.file}`
+  const fileExists = await fs.stat(`./${fileName}`).catch(() => undefined)
+  if (fileExists) {
+    const file = await fs.readFile(`${__dirname}${fileName}`)
+    const mimeType = mime.contentType(req.params.file)
+    if (mimeType) res.setHeader('Content-Type', mimeType)
+    return res.send(file)
   } else {
-    return res.status(401).json({ code: 401, message: 'Unauthorized' })
+    return res.status(404).json({ code: 404, message: "Category doesn't exist" })
   }
 })
 
@@ -45,7 +51,28 @@ app.get('/endpoints', async (req, res) => {
   })
 })
 
+app.use((req, res, next) => {
+  if (req.headers?.authorization === process.env.AUTH_TOKEN) {
+    next()
+  } else {
+    return res.status(401).json({ code: 401, message: 'Unauthorized' })
+  }
+})
+
+app.get('/nsfw/:category', async (req, res) => {
+  const dirName = `./images/nsfw/${req.params.category}`
+  const dirExists = await fs.stat(dirName).catch(() => undefined)
+  if (dirExists) {
+    const files = await fs.readdir(dirName)
+    const randomNumber = Math.round(Math.random() * await files.length)
+    return res.status(200).json({ code: 200, url: `${process.env.SUB_PATH}/nsfw/${req.params.category}/${files[randomNumber]}` })
+  } else {
+    return res.status(404).json({ code: 404, message: "Category doesn't exist" })
+  }
+})
+
 app.get('/:category', async (req, res) => {
+  console.log(req.params.category)
   const dirName = `./images/${req.params.category}`
   const dirExists = await fs.stat(dirName).catch(() => undefined)
   if (dirExists) {
